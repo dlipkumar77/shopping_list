761a788c3cc8e84c2a79653604ad37ad097828e7
deepak <dlipkumar77@gmail.com>
2025-02-10 12:11:00 +0530

    DropdownButtonFormField

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -22,11 +23,50 @@ class _NewItemState extends State<NewItem> {
             children: [
               TextFormField(
                 maxLength: 50,
-                decoration: InputDecoration(label: Text('Name')),
+                decoration: InputDecoration(
+                  label: Text('Name'),
+                ),
                 validator: (value) {
                   return 'Demo...';
                 },
-              ) // instead of TextField
+              ), // instead of TextField
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                height: 16,
+                                width: 16,
+                                color: category.value.color,
+                              ),
+                              SizedBox(
+                                width: 6,
+                              ),
+                              Text(category.value.title),
+                            ],
+                          ),
+                        )
+                    ], onChanged: (value) {}),
+                  )
+                ],
+              )
             ],
           ),
         ),
